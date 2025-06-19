4a917b360b4412ce253d132cac0e4d01a3506045
Scott <67354101+scGetStuff@users.noreply.github.com>
2025-06-19 17:10:45 -0400

    usless server running on 8080 CH1 L4

--- main.go
+++ main.go
@@ -2,8 +2,19 @@ package main
 
 import (
 	"fmt"
+	"net/http"
 )
 
 func main() {
 	fmt.Println("chirpy main()")
+	doStuff()
+}
+
+func doStuff() {
+	stuff := http.NewServeMux()
+	server := http.Server{}
+	server.Addr = ":8080"
+	server.Handler = stuff
+
+	server.ListenAndServe()
 }
