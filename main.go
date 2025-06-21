960a32bf513ceeb840f7f42996f1846c1c62a065
Scott <67354101+scGetStuff@users.noreply.github.com>
2025-06-21 17:21:16 -0400

    done CH1

--- main.go
+++ main.go
@@ -2,6 +2,7 @@ package main
 
 import (
 	"fmt"
+	"log"
 	"net/http"
 )
 
@@ -11,12 +12,31 @@ func main() {
 }
 
 func doStuff() {
+	const rootPath = "."
+	const rootPrefix = "/app"
+	const port = "8080"
 	mux := http.NewServeMux()
-	server := &http.Server{}
-	server.Addr = ":8080"
-	server.Handler = mux
+	srv := &http.Server{
+		Addr:    ":" + port,
+		Handler: mux,
+	}
 
-	mux.Handle("/", http.FileServer(http.Dir(".")))
+	// L5
+	// mux.Handle("/", http.FileServer(http.Dir(".")))
 
-	server.ListenAndServe()
+	// L11
+	mux.Handle(rootPrefix+"/",
+		http.StripPrefix(rootPrefix, http.FileServer(http.Dir(rootPath))))
+	mux.HandleFunc("/healthz", health)
+
+	log.Printf("Serving on port: %s\n", port)
+	log.Fatal(srv.ListenAndServe())
+}
+
+func health(res http.ResponseWriter, req *http.Request) {
+	// TODO: 503 Service Unavailable
+
+	res.Header().Add("Content-Type", "text/plain; charset=utf-8")
+	res.WriteHeader(200)
+	res.Write([]byte("OK"))
 }
