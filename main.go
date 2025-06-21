cb6f74eeb7fbbeebe8c918a2b43b050d651a280c
Scott <67354101+scGetStuff@users.noreply.github.com>
2025-06-21 18:04:44 -0400

    functional CH2 L1, going to refactor now

--- main.go
+++ main.go
@@ -4,7 +4,23 @@ import (
 	"fmt"
 	"log"
 	"net/http"
+	"sync/atomic"
 )
+
+type apiConfig struct {
+	fileserverHits atomic.Int32
+}
+
+func (cfg *apiConfig) middlewareMetricsInc(next http.Handler) http.Handler {
+	return http.HandlerFunc(func(w http.ResponseWriter, r *http.Request) {
+		cfg.fileserverHits.Add(1)
+		next.ServeHTTP(w, r)
+	})
+}
+
+var apiCfg = apiConfig{
+	fileserverHits: atomic.Int32{},
+}
 
 func main() {
 	fmt.Println("chirpy main()")
@@ -26,8 +42,12 @@ func doStuff() {
 
 	// L11
 	mux.Handle(rootPrefix+"/",
-		http.StripPrefix(rootPrefix, http.FileServer(http.Dir(rootPath))))
+		http.StripPrefix(rootPrefix,
+			apiCfg.middlewareMetricsInc(
+				http.FileServer(http.Dir(rootPath)))))
 	mux.HandleFunc("/healthz", health)
+	mux.HandleFunc("/metrics", count)
+	mux.HandleFunc("/reset", reset)
 
 	log.Printf("Serving on port: %s\n", port)
 	log.Fatal(srv.ListenAndServe())
@@ -39,4 +59,19 @@ func health(res http.ResponseWriter, req *http.Request) {
 	res.Header().Add("Content-Type", "text/plain; charset=utf-8")
 	res.WriteHeader(200)
 	res.Write([]byte("OK"))
+}
+
+func count(res http.ResponseWriter, req *http.Request) {
+	res.Header().Add("Content-Type", "text/plain; charset=utf-8")
+	res.WriteHeader(200)
+	s := fmt.Sprintf("Hits: %d", apiCfg.fileserverHits.Load())
+	res.Write([]byte(s))
+}
+
+func reset(res http.ResponseWriter, req *http.Request) {
+	res.Header().Add("Content-Type", "text/plain; charset=utf-8")
+	res.WriteHeader(200)
+	apiCfg.fileserverHits.Store(0)
+	s := fmt.Sprintf("reset: %d", apiCfg.fileserverHits.Load())
+	res.Write([]byte(s))
 }
